9eaae22857b0520c48a9d135fc51dc300a56a109
Hamza Memon <hamza.memon@systemsltd.com>
2024-11-22 16:30:32 +0500

    new repo

---
+++ GithubCloneService.cs
@@ -0,0 +1,51 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadScheduler.Services
+{
+    internal class GithubCloneService
+    {
+        public async Task<bool> ClonePublicRepository(string repoUrl, string localPath)
+        {
+            bool isSuccess = false;
+
+            try
+            {
+                // Ensure the master folder exists
+                if (!Directory.Exists(localPath))
+                {
+                    Directory.CreateDirectory(localPath);
+                }
+                var repoName = await GetRepositoryName(repoUrl);
+                var repoPath = Path.Combine(localPath, repoName);
+
+                // Clone the public repository without credentials
+                Repository.Clone(repoUrl, repoPath);
+
+                Console.WriteLine($"Repository cloned successfully into {repoPath}!");
+                return isSuccess = true;// Success
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error cloning repository: " + ex.Message);
+                return isSuccess = true; // Failed
+
+            }
+        }
+
+        public async Task<string> GetRepositoryName(string repoUrl)
+        {
+            // Extract the repository name from the URL
+            var uri = new Uri(repoUrl);
+            var segments = uri.Segments;
+            var lastSegment = segments[segments.Length - 1];
+            var repoName = Path.GetFileNameWithoutExtension(lastSegment);
+            return repoName;
+        }
+    }
+}
